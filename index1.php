2e02a38db642e2e3497e5826d1a976025b90cd61
ebulatova1 <47461241+ebulatova1@users.noreply.github.com>
2019-03-02 10:33:29 +0300

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,27 @@
+<?php
+	$title='Наша первая страница с PHP';
+?>
+
+
+
+
+
+
+<html>
+<head>
+	<title><?php print $title;</title>
+</head>
+<body>
+<?php
+echo "Hello World";
+//Это комментарий
+/*
+Это
+
+тоже
+
+комментарий
+*/
+?>
+</body>
+</html>
---
+++ index12.php
@@ -0,0 +1,23 @@
+<?php
+	$title='Наша первая страница с PHP';
+?>
+<html>
+<head>
+	<title><?php print $title;?></title>
+</head>
+<body>
+	Результат выполнения:
+<?php
+$newarray = array {
+1 = 'красный',
+2 = 'синий',
+3 = 'синий',
+};
+
+for ($i=1;$i<3;$i++){
+	print $i . ' ' . $newarre[i] . ' ';
+}
+print $newarrey [3];
+?>
+</body>
+</html>
